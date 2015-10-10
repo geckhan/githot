6861e50a574cdb441eb693ac5072d86e476f679d
geckhan <geckhancn@gmail.com>
2015-10-10 10:49:14 +0800

    Update Injector.java
    interface 就是抽象的

--- app/src/main/java/com/knight/arch/module/Injector.java
+++ app/src/main/java/com/knight/arch/module/Injector.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import dagger.ObjectGraph;
 
-public abstract interface Injector {
-  public abstract List<Object> getModules();
+public  interface Injector {
+  public  List<Object> getModules();
 
-  public abstract void inject(Object target);
+  public  void inject(Object target);
 
-  public abstract ObjectGraph plus(Object[] modules);
+  public  ObjectGraph plus(Object[] modules);
 }
